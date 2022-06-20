19f218ecc63b4f6799edba2e3ca19a9879b81a4f
Berk Gülşen <83736291+berkgulsen@users.noreply.github.com>
2022-06-20 14:17:10 +0300

    Admin page and Front index added to project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return view('front.index');
+});
+
+Route::prefix('admin')->name('admin.')->group(function (){
+    Route::get('panel','App\Http\Controllers\Back\Dashboard@index')->name('dashboard');
 });
